a2a40eb3a42391669eb2778bac8c11c202d1acaf
JEJU-Ratus <smh1141@naver.com>
2024-02-06 17:48:30 +0900

    feat : 추가 Nav 컴포넌트 안에 스타일 컴포넌트 생성

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import styled from'styled-components'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <NavWrapper>
+      <Logo>
+        
+      </Logo>
+    </NavWrapper>
   )
 }
 
 export default Nav
+
+const NavWrapper = styled.nav`
+  padding: 0 36px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #090b13;
+  height: 70px;
+  letter-spacing: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+padding: 0;
+width: 80px;
+margin-top: 4px;
+max-height: 70px;
+font-size: 0;
+display: inline-block;
+`
