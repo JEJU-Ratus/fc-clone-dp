a98f0000c8fb601e2e1b729e69092895ebc5089c
JEJU-Ratus <smh1141@naver.com>
2024-02-07 14:44:06 +0900

    feat : 추가 스크롤시 nav바 배경 색이 투명에서 어두운 색상으로 변경

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from'styled-components'
 
 const Nav = () => {
+
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', ()=> {
+      if(window.scrollY > 50){
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    })
+  
+    return () => {
+      window.removeEventListener('scroll',()=>{})
+    }
+  }, [])
+  
   return (
-    <NavWrapper>
+    <NavWrapper $show={show}>
       <Logo>
         <img
           alt='disney plus logo'
@@ -22,7 +39,7 @@ const NavWrapper = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #090b13;
+  background-color: ${props => props.show ? '#090b13':'transparent'};
   height: 70px;
   letter-spacing: 16px;
   position: fixed;
