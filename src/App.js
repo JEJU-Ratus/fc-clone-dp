772d68276b5530c86a80b7787677c0b22be93342
JEJU-Ratus <smh1141@naver.com>
2024-02-07 16:28:26 +0900

    feat : 추가 Container 스타일 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import styled from 'styled-components';
 import './App.css';
 import Nav from './components/Nav';
 
 function App() {
   return (
-    <>
+    <Container>
       <Nav />
-    </>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.main`
+  display: block;
+  padding : 0 clac(3.5vw + 5px);
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  position: relative;
+  top:72px;
+
+  &:after {
+    background: url("/images/home-background.png") center center / cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`
