2a37fbd655602cb594d6b691dd226feedb7d2c38
JEJU-Ratus <smh1141@naver.com>
2024-02-16 15:44:34 +0900

    feat : 추가 영화 카테고리의 내부 카테고리 정렬

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import axios from '../api/axios';
+import React, { useCallback, useEffect, useState } from 'react'
+import './Row.css'
 
-const Row = () => {
+const Row = ({title, id, fetchUrl}) => {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovieData = useCallback(async () => {
+    const res = await axios.get(fetchUrl);
+    setMovies(res.data.results);
+  },[fetchUrl])
+
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchMovieData]);
+  
+
   return (
-    <div>Row</div>
+    <section>
+      <h2>{title}</h2>
+      <div className='slider'>
+        <div className="slider__arrow-left">
+          <span className="arrow">
+            {"<"}
+          </span>
+        </div>
+        <div className="row__posters" id={id}>
+          {movies.map(movie => (
+            <img
+              className="row__poster"
+              key={movie.id} 
+              src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+              alt={movie.name} />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+            <span className="arrow">
+              {">"}
+            </span>
+        </div>
+      </div>
+    </section>
   )
 }
 
