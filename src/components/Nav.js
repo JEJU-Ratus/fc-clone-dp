37a5d346110fd70d6f9a99b0a06e7d527a224795
JEJU-Ratus <smh1141@naver.com>
2024-02-21 16:11:47 +0900

    feat : 추가 input에 따라 변하는 query의 값 및 design

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Nav = () => {
 
   const [show, setShow] = useState(false);
   const { pathname } = useLocation();
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
@@ -23,6 +25,12 @@ const Nav = () => {
     }
   }
   
+  const handelChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  }
+
+
   return (
     <NavWrapper $show={show}>
       <Logo>
@@ -33,8 +41,13 @@ const Nav = () => {
         />
       </Logo>
       { pathname === '/' ?
-        (<Login>Login</Login>) :
-        (<Input className='nav__input' type='text' placeholder='검색어를 입력하세요.'></Input>)
+        <Login>Login</Login> :
+        <Input
+          value={searchValue}
+          onChange={handelChange}
+          className='nav__input' 
+          type='text' 
+          placeholder='검색어를 입력하세요.'></Input>
       }
     </NavWrapper>
   )
@@ -43,35 +56,53 @@ const Nav = () => {
 export default Nav
 
 const Login = styled.a`
+  padding: 8px 16px;
+  border : 1px solid #f9f9f9;
+  background-color : rgba(0,0,0,0.6);
+  transition: all 0.2s ease 0s;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
 
+  &:hover {
+    border-color : transparent;
+    background-color : #f9f9f9;
+    color : gray;
+  }
 `;
 
 const Input = styled.input`
-
+  padding: 5px;
+  border: none;
+  border-radius: 5px;
+  background-color: rgba(0,0,0, 0.582);
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  color: white; 
 `;
 
 const NavWrapper = styled.nav`
-  padding: 0 36px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: ${props => props.$show ? '#090b13':'transparent'};
   height: 70px;
-  letter-spacing: 16px;
+  padding: 0 36px;
+  background-color: ${props => props.$show ? '#090b13':'transparent'};
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   z-index: 3;
+  letter-spacing: 16px;
 `;
 
 const Logo = styled.a`
-padding: 0;
-width: 80px;
-margin-top: 4px;
-max-height: 70px;
-font-size: 0;
 display: inline-block;
+width: 80px;
+max-height: 70px;
+margin-top: 4px;
+padding: 0;
+font-size: 0;
 
 img{
   width: 100%;
